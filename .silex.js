3f3bff8f5bbfe38746cedf6b5076c68bf265f8a0
Alex Hoyau <a.hoyau@silexlabs.org>
2023-08-07 11:37:44 +0200

    first draft

---
+++ .silex.js
@@ -0,0 +1,9 @@
+const DirectusStorage = require('./index')
+
+module.exports = (config, opts) => {
+  config.addStorageConnector(new DirectusStorage(config, {
+    collection: 'silex',
+    singleSiteMode: true,
+  }))
+}
+
